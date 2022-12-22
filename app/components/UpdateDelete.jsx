64a78c806390991720059f9dfe32dcbe94ae85e3
Neil <neilrowland56@yahoo.com>
2022-12-22 16:01:54 +0000

    Update Delete button taken outside of Table

--- app/components/UpdateDelete.jsx
+++ app/components/UpdateDelete.jsx
@@ -2,15 +2,21 @@
 
 import React from "react";
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleChangeAction } from "../redux/reducer";
 
 export default function UpdateDelete() {
-  const visible = useSelector((state) => state);
-  console.log(visible);
+  const visible = useSelector((state) => state.app.client.toggleForm);
+  const dispatch = useDispatch();
+
+  const onUpdate = () => {
+    dispatch(toggleChangeAction());
+    console.log(visible);
+  };
 
   return (
     <div className="flex justify-around gap-5">
-      <button className="cursor">
+      <button className="cursor" onClick={onUpdate}>
         <BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit>
       </button>
       <button className="cursor">
